3d9bf6e79c543eb3ad1e800e4afcbdd4ea70a450
nondefault <spam@nondefault.net>
2023-07-12 00:15:35 -0700

    add new modes for maximum cat fun

--- device/device.ino
+++ device/device.ino
@@ -4,8 +4,12 @@
 #define PIN_STEPPER1_STEP 7
 #define PIN_STEPPER1_DIR 8
 
-#define STEPPER_MAX_SPEED 1200.0
-#define STEPPER_ACCELERATION 2000.0
+#define STEPPER_SPEED_SNEAK 80.0
+#define STEPPER_SPEED_SWING 400.0
+#define STEPPER_SPEED_WIGGLE 800.0
+#define STEPPER_ACCELERATION_SNEAK 250.0
+#define STEPPER_ACCELERATION_SWING 500.0
+#define STEPPER_ACCELERATION_WIGGLE 1000.0
 #define STEPPER_TRAVEL_STEPS 2000
 
 AccelStepper stepper(AccelStepper::DRIVER, PIN_STEPPER1_STEP, PIN_STEPPER1_DIR);
@@ -20,16 +24,57 @@ void flash(int n) {
 }
 
 void setup() {
-  stepper.setMaxSpeed(STEPPER_MAX_SPEED);
-  stepper.setAcceleration(STEPPER_ACCELERATION);
-  pinMode(PIN_LED_BUILTIN, OUTPUT);
   flash(3);
 }
 
-void loop() {
-  // move a random amount
-  stepper.move(random(-2000, 2000));
+void wiggle() {
+  stepper.setMaxSpeed(STEPPER_SPEED_WIGGLE);
+  stepper.setAcceleration(STEPPER_ACCELERATION_WIGGLE);
+  int dir = 1;
+  int intensity = random(80, 150);
+  for (int i = random(7, 12); i > 0; --i) {
+    stepper.move(intensity * dir);
+    stepper.runToPosition();
+    dir *= -1;
+  }
+}
+
+void swing(int dir) {
+  stepper.setMaxSpeed(STEPPER_SPEED_SWING);
+  stepper.setAcceleration(STEPPER_ACCELERATION_SWING);
+  stepper.move(random(500, 2000) * dir);
   stepper.runToPosition();
-  // wait a random time
-  delay(random(200, 2000));
+}
+
+void sneak(int dir) {
+  stepper.setMaxSpeed(STEPPER_SPEED_SNEAK);
+  stepper.setAcceleration(STEPPER_ACCELERATION_SNEAK);
+  stepper.move(random(250, 1000) * dir);
+  stepper.runToPosition();
+}
+
+bool justDelayed = false;
+void loop() {
+  // choose action
+  long choice = random(3 + 3);
+  if (choice == 0) {
+    if (random(2) == 0)
+      swing(1);
+    else
+      swing(-1);
+  }
+  else if (choice == 1) {
+    wiggle();
+  }
+  else if (choice == 2) {
+    if (random(2) == 0)
+      sneak(1);
+    else
+      sneak(-1);
+  }
+  else if (!justDelayed) {
+    delay(random(2000, 20000));
+    justDelayed = true;
+  }
+  justDelayed = false;
 }
